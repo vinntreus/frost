3fec2cf7da77bb7d0c8db1c05bf2b9ce8414b685
Daniel Vinntreus <gudski@gmail.com>
2014-10-06 20:22:42 +0200

    generate id on event

--- test/adding-events.js
+++ test/adding-events.js
@@ -2,13 +2,59 @@ var expect = require('chai').expect;
 var frost = require('../lib/frost.js');
 
 describe('adding events', function(){
-    it('should store event', function(done){
-      var store = { 
-        save : function(event){ 
-          expect(event.name).to.equal('a');
-          done();
-        }
-      };
-      frost(store).add({name : 'a'});
-    });
+  var es, store, idGenerator;
+  
+  beforeEach(function(){
+    store = { save : function(){}};
+    idGenerator = { generate : function(){}};
+    es = frost({store : store, idGenerator : idGenerator});
+  });
+
+  it('should store event', function(done){
+    store.save = function(event){ 
+      expect(event.name).to.equal('a');
+      done();
+    };
+    es.add({name : 'a'});
+  });
+
+  it('should throw exception if name is missing', function(){
+    var addWithoutName = function(){
+      es.add({});
+    };
+    expect(addWithoutName).to.throw('event name');
+  });
+
+  it('should throw exception if name is null', function(){
+    var addWithNameNull = function(){
+      es.add({name : null});
+    };
+    expect(addWithNameNull).to.throw('event name');
+  });
+
+  it('should create clone of event data', function(){
+    var eventData = {name: 'a'};
+    store.save = function(event){ 
+      event.name = 'b';
+    };
+    es.add(eventData);
+    expect(eventData.name).to.equal('a');
+  });
+
+  it('should keep id if passed in', function(done){
+    store.save = function(event){ 
+      expect(event.id).to.equal(1);
+      done();
+    };
+    es.add({name : 'a', id: 1});
+  });
+
+  it('should generate id if missing', function(done){
+    idGenerator.generate = function(){ return 2;};
+    store.save = function(event){ 
+      expect(event.id).to.equal(2);
+      done();
+    };
+    es.add({name : 'a'});
+  });
 });
