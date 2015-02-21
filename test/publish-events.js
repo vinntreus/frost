3e47d896fb339858e97d6c77a20f1a2f6921bacc
Daniel Vinntreus <gudski@gmail.com>
2015-02-21 16:37:51 +0100

    publish event stores it

---
+++ test/publish-events.js
@@ -0,0 +1,20 @@
+var assert = require('chai').assert;
+var frost = require('../lib/frost.js');
+
+describe('publish events', function(){
+
+  it('stores event', function(){
+    var es = frost({clearStore: true});
+    var events = [];
+    es.registerHandler({a : function(event, domain){
+      domain.a = event.data;
+    }});
+
+    es.publishEvent({name: 'a', key:'root', id: 1, data : 2});
+    events = es.getEventsFrom('root');
+
+    assert.equal(events.length, 1);
+    assert.equal(events[0].name, 'a');
+  });
+
+});
