460f2334bcbcae65eec5da579488db9acb011315
Daniel Vinntreus <gudski@gmail.com>
2015-03-01 09:06:21 +0100

    extend frost to be able to use localstorage-store

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports = require('./lib/frost');
+var localstorageStore = require('./localstorage-store');
+var frost = require('./lib/frost');
+
+frost.useLocalStorage = function(){
+  frost.setStore(localstorageStore);
+};
+
+module.exports = frost;
